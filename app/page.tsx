81e70868a50c6faa45fa57ed18727468364ceecd
Tanisha2174 <tpanesar2174@gmail.com>
2025-02-07 12:03:26 +0530

    mobile view

--- app/page.tsx
+++ app/page.tsx
@@ -50,7 +50,7 @@ export default function Home() {
         <img
           src="/images/legsim_mobile.png"
           alt="Legsim Logo"
-          className="w-[90%] sm:w-[80%] md:hidden h-auto"  // Increased width
+          className="w-[100%] sm:w-[100%] md:hidden h-auto"  // Increased width
         />
 
         {/* Desktop Logo Image */}
